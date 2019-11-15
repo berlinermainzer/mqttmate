5a18f795029151c32e3ec038b7e0e30f2b90faf1
Johannes Felten <johannesfelten@gmail.com>
2019-11-15 15:42:54 +0100

    Initial setup

---
+++ src/main/java/de/felten/mqtt/MqttMate/MqttMateApplication.java
@@ -0,0 +1,13 @@
+package de.felten.mqtt.MqttMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/felten/mqtt/MqttMate/MqttMateApplicationTests.java
@@ -0,0 +1,13 @@
+package de.felten.mqtt.MqttMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
