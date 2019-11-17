75237b0b81fd9f7460771de9d1b918f97f94313c
Johannes Felten <johannesfelten@gmail.com>
2019-11-17 11:34:02 +0100

    Disable Security for now.

--- src/main/java/de/felten/mqtt/MqttMate/MqttMateApplication.java
+++ src/main/java/de/felten/mqtt/MqttMate/MqttMateApplication.java
@@ -1,9 +1,12 @@
 package de.felten.mqtt.MqttMate;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class MqttMateApplication {
 
 	public static void main(String[] args) {
