6034ee74adbfb4e0d227e7ce199b06f21fbcea97
Johannes Felten <johannesfelten@gmail.com>
2019-11-24 17:43:09 +0100

    Query api enhanced, add external config

--- src/main/java/de/felten/mqtt/MqttMate/repository/TemperatureRepository.java
+++ src/main/java/de/felten/mqtt/MqttMate/repository/TemperatureRepository.java
@@ -1,6 +1,8 @@
 package de.felten.mqtt.MqttMate.repository;
 
 import de.felten.mqtt.MqttMate.model.Temperature;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -10,6 +12,8 @@ import java.util.List;
 public interface TemperatureRepository extends MongoRepository<Temperature, String>, PagingAndSortingRepository<Temperature, String> {
 
     List<Temperature> findByDevice(String device);
+
+    Page<Temperature> findByDevice(String device, Pageable pageable);
 
     List<Temperature> findByTimestampUtcBetween(LocalDateTime timestampGT, LocalDateTime timestampLT);
 
